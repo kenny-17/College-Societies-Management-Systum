4677c0a5a0d088a57f792b12aeec8b625198547f
kanishk <134843160+kenny-17@users.noreply.github.com>
2024-06-16 00:30:01 +0530

    Update dashboard.php

--- college societies management system/dashboard.php
+++ college societies management system/dashboard.php
@@ -4,7 +4,7 @@ session_start();
 // Check if the user is logged in
 if (!isset($_SESSION["user_id"])) {
     // Redirect to the login page if not logged in
-    header("Location: login.html");
+    header("Location: index.html");
     exit();
 }
 
